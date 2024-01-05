591b29e67bdac5d457fd392322f8941f0d6c293b
Pranjal826 <pranjalshukla245@gmail.com>
2024-01-05 19:17:22 +0530

    u

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const port = 5100;
 emitter.setMaxListeners(15);
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://myfolio-frontend.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'https://myfolio-frontend-chk58pxv8-pranjal-shuklas-projects.vercel.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
